3ea1901b99eedcbc8ddb260ac350c215faef688f
Alexey Kuznetchov <Kuznetchovav@gmail.com>
2023-04-10 15:15:59 +0300

    Added program file

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует 
+// массив из строк, длина которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+Console.WriteLine("Enter the words separated by a space into a line:");
+string String = Console.ReadLine()!;
+string[] array = String.Split(" ");
+int size = SortArray(array);
+
+
+int SortArray(string[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+string[] NewArray(int size)
+{
+    string[] ArrayResult = new string[size];
+    int i = 0;
+    int j = 0;
+    while (i < array.Length)
+    {
+        if (array[i].Length <= 3)
+        {
+            ArrayResult[j] = array[i];
+            j++;
+        }
+        i++;
+    }
+    return ArrayResult;
+
+}
+
+Console.WriteLine($" [{String.Join(", ", NewArray(size))}]");
+Console.ReadLine();
+
